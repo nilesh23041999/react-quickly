b9b41c6587873678cef27493fc953f2c3076ca78
nilesh23041999 <nileshtiwari2399@gmail.com>
2024-07-31 14:26:33 +0530

    ep02 commit

---
+++ ep-02/app.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const heading = React.createElement("h1", {}, "Hello from React JSJSS");
+const root = document.getElementById("root");
+ReactDOM.render(heading, root);
+
